d293c2865c3d3cdffc0bcefdd831326dc43f5762
rena0204 <rena0204@sookmyung.ac.kr>
2025-11-13 15:19:43 +0900

    11 (2)

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+int i = 300; 
+
+int *pi = &i; 
+char *pc = &i; 
+
+printf("%i, %i, %i\n", i, *pi, *pc);
+
+	return 0;
+}
